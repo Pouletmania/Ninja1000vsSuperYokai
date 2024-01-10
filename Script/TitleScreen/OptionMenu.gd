0e2dbc444304bc166c5cf1f8001ca9d958140465
Guillaume Masson <guillaume.masson42@gmail.com>
2024-01-10 02:27:54 +0100

    TitleScreen
    Mise en page du menu principal
    Test de navigation de menu (Option -> Continu)

---
+++ Script/TitleScreen/OptionMenu.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_option_pressed():
+	self.visible = true
+
+
+func _on_back_pressed():
+	self.visible = false
