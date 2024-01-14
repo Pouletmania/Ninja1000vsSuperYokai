b47778ae6da1ab4e57466833c6358f8248759681
Guillaume Masson <guillaume.masson42@gmail.com>
2024-01-14 18:06:38 +0100

    Simplification de la contruction du GridInput

--- Script/TitleScreen/InputTab/GridInput.gd
+++ Script/TitleScreen/InputTab/GridInput.gd
@@ -5,16 +5,15 @@ var Config = ConfigFile.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if try_to_load_config_files() and Config.has_section_key("InputConfig", "AutoLoad"):
-		build_grid(Config.get_value("InputConfig", "AutoLoad"))
-	else:
-		build_grid(false)
+	load_current_config()
+	build_grid()
 
-func build_grid(fromConfig: bool):
-	if fromConfig:
-		build_from_config()
-	else:
-		build_from_input_map()
+func build_grid():
+	for action in InputMap.get_actions():
+		if not action.begins_with("ui_") and Config.has_section_key("Input", action):
+			add_action_combo(action, Config.get_value("Input", action))
+		elif not action.begins_with("ui_"):
+			add_action_combo(action, InputMap.action_get_events(action)[0].as_text())
 
 func try_to_load_config_files():
 	if load_current_config() != OK:
@@ -27,44 +26,11 @@ func load_current_config():
 
 func rebuild_grid(fromConfig: bool):
 	clear_grid()
-	build_grid(fromConfig)
+	build_grid()
 
 func clear_grid():
 	for children in get_children():
 		remove_child(children)
-
-func build_from_config():
-	if check_input_config():
-		for actionKey in Config.get_section_keys("Input"):
-			if not Config.get_value("Input", actionKey).begins_with("ui_"):
-				add_action_combo(actionKey, Config.get_value("Input", actionKey))
-
-func check_input_config():
-	check_config_to_inputmap()
-	if check_inputmap_to_config():
-		return true
-	else:
-		return false
-
-func check_inputmap_to_config():
-	for action in InputMap.get_actions():
-		if not action.begins_with("ui_") and not Config.has_section_key("Input", action):
-			print("Warning ! Erreur fichier de configuration : action " + action + " manquante")
-			print("Une nouvelle sauvegarde est nécessaire pour le bon fonctionnement du jeu")
-			build_from_input_map()
-			return false
-	return true
-
-func check_config_to_inputmap():
-	for action in Config.get_section_keys("Input"):
-		if not InputMap.has_action(action):
-			Config.erase_section_key("Input", action)
-			Config.save("res://Config/ConfigFiles.cfg")
-
-func build_from_input_map():
-	for action in InputMap.get_actions():
-		if not action.begins_with("ui_"):
-			add_action_combo(action, InputMap.action_get_events(action)[0].as_text())
 
 func add_action_combo(action: String, input: String):
 	var actionCombo = InputConfigurationInstance.instantiate()
