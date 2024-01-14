02749967fe3bc6e8c74b8994aa895dd386e03726
Guillaume Masson <guillaume.masson42@gmail.com>
2024-01-14 12:00:57 +0100

    Save current config
    Implementation du bouton sauvegardant la config des input

---
+++ Script/TitleScreen/InputMenu.gd
@@ -0,0 +1,33 @@
+extends VBoxContainer
+
+var Config = ConfigFile.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_save_button_up():
+	for action in InputMap.get_actions():
+		if not action.begins_with("ui_"):
+			Config.set_value("Input", action, InputMap.action_get_events(action)[0].as_text())
+	Config.save("res://Config/ConfigFiles.cfg")
+
+
+
+func _on_load_button_up():
+	var err = Config.load("res://Config/ConfigFiles.cfg")
+	
+	# If the file didn't load, ignore it.
+	if err != OK:
+		print("Error : No config Files load")
+	else:
+		print("Load Config")
+	
+	
+
