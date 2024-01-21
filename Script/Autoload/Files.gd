e448c29d10413eb95784f47d8f41c8424af0b1ec
Guillaume Masson <guillaume.masson42@gmail.com>
2024-01-21 22:12:28 +0100

    Création autoload Files
    Création autoload Files contenant servant de "BDD"

---
+++ Script/Autoload/Files.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var TitleScreenScene = preload("res://Scene/TitleScreen/TitleScreen.tscn")
+var NewGameScene = preload("res://Scene/SandBox.tscn")
--- Script/Main/Main.gd
+++ Script/Main/Main.gd
@@ -6,13 +6,8 @@ extends Node
 #Script attaché au node Main
 #Point d'entrée de l'execution du jeu après la génération des nodes Autoload
 
-#---------------#
-#	Variable
-#---------------#
-var FirstScene = preload("res://Scene/TitleScreen/TitleScreen.tscn").instantiate()
-
 #----------				----------#
 #	Ready + fonctions associés
 #----------				----------#
 func _ready():
-	add_child(FirstScene)
+	add_child(Files.TitleScreenScene.instantiate())
